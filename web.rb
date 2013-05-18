ce13ba372c03aaebc3ec5c2ea1aa7a9aa785bcf8
Jerry D'Antonio <stumpjumper@gmail.com>
2013-05-18 19:39:10 -0400

    Better error reporting

--- web.rb
+++ web.rb
@@ -17,8 +17,6 @@ end
 
 get '/counts/:concept_id' do
 
-  html = ''
-
   concept_id = URI.decode(params[:concept_id])
   index = $SF1.index{|concept| concept['name'] =~ /^#{concept_id}\./}
 
@@ -27,70 +25,24 @@ get '/counts/:concept_id' do
   client = CensusApi::Client.new(API_KEY)
 
   concept = $SF1[index]
-  #pp concept
-  #html += "<h2 class=\"featurette-heading\">Census Data Set '" + concept['name'] + "' for Ohio</h2>"
-
   fields = concept['variable'].collect{|var| var['name']}.join(',')
-  #pp fields
-  data = client.sf1(fields, 'STATE:39').first
-  #pp data
-  #html += '<p><ul>'
-  #concept['variable'].each do |var|
-    ##html+= "<li>#{var['content']} for #{data['name']} is #{format_number(data[var['name']])}</li>"
-    #html+= "<li>#{var['content']} is #{format_number(data[var['name']])}</li>"
-  #end
-  #html += '</ul></p>'
+  data = client.sf1(fields, 'STATE:39')
 
-  erb(:counts, layout: true, locals: {name: concept['name'], variables: concept['variable'], data: data})
+  if data.is_a?(Hash)
+    erb(:error, layout: true, locals: {api_request: concept['name'], api_response: data, request_fields: concept['variable']})
+  else
+    erb(:counts, layout: true, locals: {name: concept['name'], variables: concept['variable'], data: data.first})
+  end
 end
 
 get '/' do
 
-  sample_size = 10
-  html = ''
+  #sample_size = 10
+  #R.eval <<-REVAL
+    #x <- rnorm(#{sample_size})
+    #avg <- mean(x)
+    #stddev <- sd(x)
+  #REVAL
 
-  #html += "<p>Running R code...</p>"
-
-  begin
-
-    #R.eval <<-REVAL
-      #x <- rnorm(#{sample_size})
-      #avg <- mean(x)
-      #stddev <- sd(x)
-    #REVAL
-
-    #html += "<p>Suceeded running R code...</p>"
-    #html += "<p>The sample is #{R.x}</p>"
-    #html += "<p>The mean is #{R.avg}</p>"
-    #html += "<p>The standard deviation is #{R.stddev}</p>"
-
-    #html += "<p>Retrieving Census XML data...</p>"
-    html += "<p><ul>"
-    $SF1.each do |concept|
-      name = concept['name']
-      if name =~ /([A-Z]\d+)\./
-        html += "<li><a href='" + url("/counts/#{$1}") + "'>" + name + '</a></li>'
-        #html += "<li><a href='" + url("/counts/#{URI.encode(name)}") + "'>" + name + '</a></li>'
-      end
-    end
-    html += "</ul></p>"
-    #html += "<p>Succeeded in retrieving Census XML data...</p>"
-
-    #html += "<p>Accessing the Census API...</p>"
-
-    #client = CensusApi::Client.new(API_KEY)
-    #ohio = client.sf1('P0010001', 'STATE:39')
-    #population = ohio.first['P0010001'].reverse.gsub(/([0-9]{3}(?=([0-9])))/, "\\1#{','}").reverse
-    #html += "<p>In 2010 there were #{population} people in Ohio.</p>"
-
-    #html += "<p>Succeeded in accessing the Census API...</p>"
-
-  #rescue Exception => e
-    #html += "<p>Danger, Will Robinson!</p>"
-    #html += "<p>#{e.class} - #{e.message}</p>"
-  #ensure
-    ##R.quit
-  end
-
-  erb(html, layout: true)
+  erb(:index, layout: true, locals: {concepts: $SF1})
 end
