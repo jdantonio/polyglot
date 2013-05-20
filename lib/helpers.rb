ec0dbdf37e1915134dafc3389bd87aa970c30cee
Jerry D'Antonio <stumpjumper@gmail.com>
2013-05-19 23:50:16 -0400

    Added descriptive statistics for counties

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,7 +1,13 @@
 module Helpers
 
   def format_number(number)
-    number.to_s.reverse.gsub(/([0-9]{3}(?=([0-9])))/, "\\1#{','}").reverse
+    whole, decimal = number.to_s.split('.')
+    whole = whole.to_s.reverse.gsub(/([0-9]{3}(?=([0-9])))/, "\\1#{','}").reverse
+    if decimal.nil?
+      return whole
+    else
+      return whole + '.' + decimal
+    end
   end
 
 end
--- web.rb
+++ web.rb
@@ -2,12 +2,14 @@ require 'sinatra'
 require 'sinatra/partial'
 
 require 'census_api'
-require 'rinruby'
 require 'active_support/inflector'
 
 require_relative 'lib/globals'
 require_relative 'lib/geography'
 require_relative 'lib/helpers'
+
+require 'rinruby'
+R.quit # kill global instance of R
 
 helpers Helpers
 
@@ -15,14 +17,6 @@ set :partial_template_engine, :erb
 enable :partial_underscores
 
 get '/' do
-
-  #sample_size = 10
-  #R.eval <<-REVAL
-  #x <- rnorm(#{sample_size})
-  #avg <- mean(x)
-  #stddev <- sd(x)
-  #REVAL
-
   erb(:index, layout: true, locals: {concepts: Globals::SF1})
 end
 
@@ -64,5 +58,66 @@ post '/totals-for-state' do
     erb(:error, layout: true, locals: {api_request: concept['name'], api_response: data, request_fields: concept['variable']})
   else
     erb(:counts, layout: true, locals: {name: concept['name'], state: Geography::STATE[state], variables: concept['variable'], data: data.first})
+  end
+end
+
+post '/stats-by-county' do
+
+  concept_id = params[:concept]
+  state = params[:state]
+  index = Globals::SF1.index{|concept| concept['name'] =~ /^#{concept_id}\./}
+
+  halt(404) if index.nil?
+
+  client = CensusApi::Client.new(Globals::API_KEY)
+
+  concept = Globals::SF1[index]
+  variable = concept['variable'].first['name']
+  data = client.sf1(variable, 'COUNTY', "STATE:#{state}")
+
+  if data.is_a?(Hash)
+    erb(:error, layout: true, locals: {api_request: concept['name'], api_response: data, request_fields: concept['variable']})
+  else
+    populations = data.collect{|county| county[variable].to_i}
+
+    r = RinRuby.new(false, false)
+    r.assign('pop', populations)
+    r.eval <<-REVAL
+      library(pastecs)
+      pop_summary <- as.numeric(summary(pop))
+      pop_fivenum <- as.numeric(fivenum(pop))
+      pop_mean <- as.numeric(mean(pop))
+      pop_sd <- as.numeric(sd(pop))
+      pop_var <- as.numeric(var(pop))
+      pop_min <- as.numeric(min(pop))
+      pop_max <- as.numeric(max(pop))
+      pop_median <- as.numeric(median(pop))
+      pop_range <- as.numeric(range(pop))
+      pop_quantile <- as.numeric(quantile(pop))
+    REVAL
+
+    stats = {
+      summary: r.pop_summary,
+      fivenum: r.pop_fivenum,
+      mean: r.pop_mean,
+      sd: r.pop_sd,
+      var: r.pop_var,
+      min: r.pop_min,
+      max: r.pop_max,
+      median: r.pop_median,
+      range: r.pop_range,
+      quantile: r.pop_quantile
+    }
+    r.quit
+    r = nil
+
+    locals = {
+      name: concept['name'],
+      variable: variable,
+      state: Geography::STATE[state],
+      data: data,
+      stats: stats
+    }
+    erb(:counties, layout: true, locals: locals)
   end
 end
