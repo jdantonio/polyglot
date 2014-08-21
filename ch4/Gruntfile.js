719096adc9039421cbc28593d99df51564365f6e
Jerry D'Antonio <stumpjumper@gmail.com>
2014-08-21 05:17:40 -0400

    wip: Working exercise 2 on p. 36

--- ch4/Gruntfile.js
+++ ch4/Gruntfile.js
@@ -5,7 +5,8 @@ module.exports = function(grunt) {
     mainFile: 'dist/Main.js',
     psc: {
       options: {
-        modules: ['Data']
+        main: 'Data.Recursion',
+        modules: ['Data.Recursion']
       },
       all: {
         src: ['<%=srcFiles%>'],
@@ -22,5 +23,5 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-purescript');
   grunt.loadNpmTasks('grunt-execute');
   grunt.registerTask('default', ['psc:all', 'dotPsci']);
-  grunt.registerTask('run', ['execute:main']);
+  grunt.registerTask('run', ['default', 'execute:main']);
 };
--- ch4/src/Data/Recursion.purs
+++ ch4/src/Data/Recursion.purs
@@ -1,6 +1,8 @@
 module Data.Recursion where
+import Data.Maybe
 import Data.Array (null)
 import Data.Array.Unsafe (head, tail)
+import Debug.Trace
 
 length :: forall a. [a] -> Number
 length arr =
@@ -17,16 +19,22 @@ fib 0 = 1
 fib 1 = 1
 fib n = fib (n - 1) + fib (n - 2)
 
+(..) :: Number -> Number -> [Number]
+(..) = Data.Array.range
+
 isEven :: Number -> Boolean
 isEven n =
   if n % 2 == 0
      then true
      else false
 
---countEven :: forall a. [a] -> Number
---countEven arr =
-  --if null arr
-     --then 0
-     --else if isEven (head arr)
-     --then 1 + countEven (tail arr)
-     --else countEven (tail arr)
+countEven :: forall a. [a] -> Number
+countEven [] = 0
+countEven arr =
+  --if 1 == head arr
+  if 1 == head [100]
+     then 1
+     else 2
+
+main = print (countEven [1,2,3,4,5,6,7,8,9,10])
+--main = print (countEven [])
