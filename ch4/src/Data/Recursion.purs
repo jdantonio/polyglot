bb5666cb0f06774e1c7b10d4e4e8ff7edc76b010
Jerry D'Antonio <stumpjumper@gmail.com>
2014-08-23 00:36:33 -0400

    More stuff from chapter 4

--- ch4/src/Data/Recursion.purs
+++ ch4/src/Data/Recursion.purs
@@ -1,14 +1,14 @@
 module Data.Recursion where
 import Data.Maybe
-import Data.Array (filter, map, null)
-import Data.Array.Unsafe (head, tail)
+import Data.Array (concatMap, filter, map, null)
+import Data.Array.Unsafe (head, last, tail)
 import Debug.Trace
 
-length :: forall a. [a] -> Number
-length arr =
-  if null arr
-     then 0
-     else 1 + length (tail arr)
+--length :: forall a. [a] -> Number
+--length arr =
+--  if null arr
+--     then 0
+--     else 1 + length (tail arr)
 
 fact :: Number -> Number
 fact 0 = 1
@@ -40,14 +40,23 @@ isEven n = isEven (n % 2)
 --     then 1
 --     else 2
 
-sq :: forall a. [Number] -> [Number]
-sq arr = map (\n -> n * n) arr
-
-(<$?>) fn arr = filter fn arr
+square :: forall a. [Number] -> [Number]
+square arr = map (\n -> n * n) arr
 
 infix 1 <$?>
 
-pos :: forall a. [Number] -> [Number]
-pos arr = (\n -> n >= 0) <$?> arr
+(<$?>) fn arr = filter fn arr
 
-main = print $ pos (-5..5)
+isPositive :: forall a. [Number] -> [Number]
+isPositive arr = (\n -> n >= 0) <$?> arr
+
+product :: forall a. [Number] -> Number
+product pair = (head pair) * (last pair) 
+
+pairs :: Number -> [[Number]]
+pairs n = concatMap (\i -> map (\j -> [i, j]) (i..n)) (1..n)
+
+factors :: Number -> [[Number]]
+factors n = filter (\pair -> product pair == n) (pairs n)
+
+main = print $ factors 10
