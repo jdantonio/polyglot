392d736d98f2f47c1d404cadcf894785c3af2c29
Jerry D'Antonio <stumpjumper@gmail.com>
2014-10-06 20:40:34 -0400

    Minimal project. Works but looks horrible.

---
+++ rethink_reddit.rb
@@ -0,0 +1,31 @@
+require 'sinatra'
+require 'rest_client'
+require 'json'
+require 'ostruct'
+require 'pp'
+
+BASE_URL = 'http://www.reddit.com'
+
+def popular_subreddits
+  response = RestClient.get(BASE_URL + '/subreddits/popular.json', {:accept => :json})
+  JSON.parse(response)['data']['children'].collect do |sub|
+    display_name = sub['data']['display_name']
+    OpenStruct.new(display_name: display_name,
+                   url: BASE_URL + sub['data']['url'],
+                   hot: hot_topics(display_name, 4))
+  end
+end
+
+def hot_topics(subreddit, limit = nil)
+  count = limit.nil? ? '' : "?limit=#{limit.to_i}"
+  response = RestClient.get(BASE_URL + "/r/#{subreddit}/hot.json#{count}", {:accept => :json})
+
+  JSON.parse(response)['data']['children'].collect do |topic|
+    OpenStruct.new(title: topic['data']['title'],
+                   url: topic['data']['url'])
+  end
+end
+
+get '/' do
+  erb :index, locals: { subreddits: popular_subreddits }
+end
