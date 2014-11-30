18bf2550742f8dc85f02c8ce3d1a88efad6a3d71
Jerry D'Antonio <stumpjumper@gmail.com>
2014-11-30 17:29:05 -0500

    Sinatra app now serves the backbone tutorial index page.

--- backbonetutorials.com/app.rb
+++ backbonetutorials.com/app.rb
@@ -2,5 +2,43 @@ require 'sinatra'
 
 class BackboneTutorial < Sinatra::Base
 
+  # GET	/users	users#index	display a list of all users
+  # GET	/users/new	users#new	return an HTML form for creating a new user
+  # POST	/users	users#create	create a new user
+  # GET	/users/:id	users#show	display a specific user
+  # GET	/users/:id/edit	users#edit	return an HTML form for editing a user
+  # PATCH/PUT	/users/:id	users#update	update a specific user
+  # DELETE	/users/:id	users#destroy	delete a specific user
 
+  get '/', provides: :html do
+    File.open('./index.html', 'r').read
+  end
+
+  get '/users', provides: :json do
+    content_type :json
+  end
+
+  get '/users/new', provides: :json do
+    content_type :json
+  end
+
+  post '/users/', provides: :json do
+    content_type :json
+  end
+
+  get '/users/:id', provides: :json do
+    content_type :json
+  end
+
+  get '/users/:id/edit', provides: :json do
+    content_type :json
+  end
+
+  put '/users/:id', provides: :json do
+    content_type :json
+  end
+
+  delete '/users/:id', provides: :json do
+    content_type :json
+  end
 end
